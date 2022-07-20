33e5338fce62d24b9213a393db706553153d7cdb
laurent <laurent@sutterlity.studio>
2022-07-20 10:27:22 +0200

    feat: add textarea component

---
+++ packages/app/components/design-system/base/Textarea/story.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import Textarea from ".";
+
+export default {
+  title: "Component/Textarea",
+  component: Textarea,
+  args: {
+    placeholder: "Placeholder",
+  },
+} as ComponentMeta<typeof Textarea>;
+
+export const Base: ComponentStory<typeof Textarea> = (args) => <Textarea {...args} />;
+
+export const isInvalid: ComponentStory<typeof Textarea> = (args) => (
+  <Textarea {...args} isInvalid />
+);
+
+export const isValid: ComponentStory<typeof Textarea> = (args) => (
+  <Textarea {...args} isValid />
+);
+
+export const isDisabled: ComponentStory<typeof Textarea> = (args) => (
+  <Textarea {...args} isDisabled />
+);
+
+export const isReadOnly: ComponentStory<typeof Textarea> = (args) => (
+  <Textarea {...args} isReadOnly />
+);
